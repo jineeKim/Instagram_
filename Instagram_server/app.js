c4ad549259644d4829f95b86a7db0757e065496b
김진희 <jin03hee16@gmail.com>
2019-07-30 17:37:57 +0900

    feat: facebook  login, response  token

--- Instagram_server/app.js
+++ Instagram_server/app.js
@@ -4,10 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session'); // 세션 설정
+const passport = require('passport');
+const passportConfig = require('./config/passport');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+//passport setup
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false })); // 세션 활성화
+app.use(passport.initialize()); // passport 구동 (꼭 필요)
+app.use(passport.session()); // 세션 연결 (꼭 필요)
+passportConfig(); // 이 부분 추가
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Instagram_server/routes/auth/index.js
+++ Instagram_server/routes/auth/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/auth', require('./auth/index'));
+router.use('/', require('./passportTest'));
 
 module.exports = router;
