8a83d81b4375028d8796834924f175998533cf21
김진희 <jin03hee16@gmail.com>
2019-07-31 20:01:25 +0900

    fix: add userIdx when creating token feat: read user data, update user id and profile image

--- Instagram_server/routes/index.js
+++ Instagram_server/routes/index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/auth', require('./auth/index'));
+router.use('/myPage', require('./myPage'));
+router.use('/image', require('./image'));
 
 module.exports = router;
---
+++ Instagram_server/routes/myPage/mypage.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+const resMessage = require('../../module/utils/responseMessage');
+const statusCode = require('../../module/utils/statusCode');
+const utils = require('../../module/utils/utils');
+
+const pool = require('../../module/pool');
+const jwt = require('../../module/jwt');
+
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
+
+router.patch('/id', async (req, res) => {
+    const user = jwt.verify(req.headers.token);
+
+    if (user === null || !req.body.id) {
+        res.status(200).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    } else {
+        const selectIdQuery = 'SELECT id FROM user WHERE id = ';
+        const result = await pool.queryParam_Parse(selectIdQuery, [req.body.id]);
+
+        if (result === null) {
+            const updateUserQuery = 'UPDATE user SET id = ? WHERE userIdx = ?';
+            await pool.queryParam_Parse(updateUserQuery, [req.body.id, user.userIdx]);
+            res.status(200).send(utils.successTrue(statusCode.CREATED, resMessage.UPDATE_SUCCESS));
+        }else{
+            res.status(200).send(utils.successFalse(statusCode.FORBIDDEN, resMessage.DUPLICATION_NICKNAME));
+        }
+
+    }
+});
+
+router.patch('/profile', async (req, res) => {
+    const user = jwt.verify(req.headers.token);
+
+    if (user === null || !req.body.profile) {
+        res.status(200).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    } else {
+        const updateUserQuery = 'UPDATE user SET img = ? WHERE userIdx = ?';
+        await pool.queryParam_Parse(updateUserQuery, [req.body.profile, user.userIdx]);
+
+        res.status(200).send(utils.successTrue(statusCode.CREATED, resMessage.UPDATE_SUCCESS));
+    }
+});
+
+router.get('/user', async (req, res) => {
+    const user = jwt.verify(req.headers.token);
+
+    if (user === null) {
+        res.status(200).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    } else {
+        const selectUserQuery = 'SELECT * FROM user WHERE userIdx = ?';
+        const result = await pool.queryParam_Parse(selectUserQuery, [user.userIdx]);
+
+        console.log("userIdx:::" + JSON.stringify(user));
+
+        delete result[0].userIdx;
+        delete result[0].authType;
+        delete result[0].pw;
+
+        res.status(200).send(utils.successTrue(statusCode.CREATED, resMessage.READ_SUCCESS, result));
+    }
+});
+
+module.exports = router;
