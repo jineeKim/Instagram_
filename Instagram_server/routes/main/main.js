e3a69cdc1830b7c2704ba7e4b94374179e9d982a
김진희 <jin03hee16@gmail.com>
2019-08-24 00:12:11 +0900

    style: db 연동 module 작업한 코드로 스타일 변경

--- Instagram_server/routes/main/main.js
+++ Instagram_server/routes/main/main.js
@@ -6,8 +6,7 @@ const statusCode = require('../../module/utils/statusCode');
 const utils = require('../../module/utils/utils');
 const upload = require('../../config/multer');
 
-const pool = require('../../config/dbConfig');
-const db = require('../../module/pool');
+const pool = require('../../module/pool');
 const jwt = require('../../module/jwt');
 
 var moment = require('moment');
@@ -20,50 +19,34 @@ router.get('/', async (req, res) => {
     if (user === null || !req.headers.token) {
         res.status(200).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
     } else {
-        try {
-            var connection = await pool.getConnection();
-            const selectUserPost =
-            'SELECT * FROM '+
-                '(SELECT P.*, COUNT (L.likeIdx) likeCnt, COUNT(C.commentIdx) commentCount ' +
-                'FROM post AS P ' +
-                'LEFT OUTER JOIN instagram.like AS L ON P.postIdx = L.postIdx ' +
-                'LEFT OUTER JOIN comment AS C ON P.postIdx = C.postIdx ' +
-                'LEFT OUTER JOIN follow AS F ON P.userIdx = F.followingIdx ' +
-                'WHERE F.userIdx = ? ' +
-                'GROUP BY P.postIdx ' +
-                //'ORDER BY P.postIdx DESC '+
-                'UNION '+
+        const selectUserPost =
+            'SELECT * FROM ' +
+            '(SELECT P.*, COUNT (L.likeIdx) likeCnt, COUNT(C.commentIdx) commentCount ' +
+            'FROM post AS P ' +
+            'LEFT OUTER JOIN instagram.like AS L ON P.postIdx = L.postIdx ' +
+            'LEFT OUTER JOIN comment AS C ON P.postIdx = C.postIdx ' +
+            'LEFT OUTER JOIN follow AS F ON P.userIdx = F.followingIdx ' +
+            'WHERE F.userIdx = ? ' +
+            'GROUP BY P.postIdx ' +
+            'UNION ' +
 
-                'SELECT P.*, COUNT (L.likeIdx) likeCnt, COUNT(C.commentIdx) commentCount ' +
-                'FROM post AS P ' +
-                'LEFT OUTER JOIN instagram.like AS L ON P.postIdx = L.postIdx ' +
-                'LEFT OUTER JOIN comment AS C ON P.postIdx = C.postIdx ' +
-                //'LEFT OUTER JOIN follow AS F ON P.userIdx = F.followingIdx ' +
-                'WHERE P.userIdx = ? ' +
-                'GROUP BY P.postIdx) c ' +
-                'ORDER BY postIdx DESC '  ;
+            'SELECT P.*, COUNT (L.likeIdx) likeCnt, COUNT(C.commentIdx) commentCount ' +
+            'FROM post AS P ' +
+            'LEFT OUTER JOIN instagram.like AS L ON P.postIdx = L.postIdx ' +
+            'LEFT OUTER JOIN comment AS C ON P.postIdx = C.postIdx ' +
+            'WHERE P.userIdx = ? ' +
+            'GROUP BY P.postIdx) c ' +
+            'ORDER BY postIdx DESC ';
 
+        var postResult = await pool.queryParam_Parse(selectUserPost, [user.userIdx, user.userIdx]);
 
-            // 'SELECT post.* ' +
-            // 'FROM post ' +
-            // 'INNER JOIN follow ' +
-            // 'ON post.userIdx = follow.followingIdx ' +
-            // 'WHERE follow.followingIdx = ? ' +
-            // 'UNION ' +
-            // 'SELECT posts.* ' +
-            // 'FROM posts ' +
-            // 'WHERE posts.user_id = ? ';
-
-            var postResult = await connection.query(selectUserPost, [user.userIdx, user.userIdx]);
-        } catch (err) {
-            console.log(err);
-            connection.rollback(() => { });
+        if (!postResult) {
             res.status(200).send(utils.successFalse(statusCode.DB_ERROR, resMessage.READ_FAIL));
-        } finally {
-
-            pool.releaseConnection(connection);
+        } else {
             res.status(200).send(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, postResult));
         }
+
+
     }
 });
 
